d005c8ac6bd928707a8cf4122cc45dac594ff864
Nathan Kinkade <nkkinkade@gmail.com>
2020-07-22 10:07:28 -0500

    finished challenges

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -25,9 +25,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  let a = 'This is my story.';
-  str = a.concat(' The end.');
-  return str
+  str = str.concat(' The end.');
+  return str;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -44,7 +43,8 @@ console.log(a) prints [1, 2, 3, 1]
 ------------------------------------------------------------------------------------------------ */
 
 const appendFirstToLast = (arr) => {
-  // Solution code here...
+  let a = arr[0];
+  arr.push(a);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,7 +63,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -84,7 +84,7 @@ const setStatusAsAuthor = (people) => {
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 6 - Stretch Goal
+CHALLENGE 6 - Stret addch Goal
 
 Write a function that accepts two arrays. Append the values from the second array into the first,
 maintaining the ordering.
@@ -119,52 +119,42 @@ describe('Testing challenge 1', () => {
     expect(raisedToTheThird([2, 4, 5, -7, 0])).toStrictEqual([8, 64, 125, -343, 0]);
   });
 });
-
 describe('Testing challenge 2', () => {
   test('It should append without modifying the oiginal', () => {
     const a = 'This is my story.';
     const b = appendTheEnd(a);
-
     expect(a).toStrictEqual('This is my story.');
     expect(b).toStrictEqual('This is my story. The end.');
   });
 });
-
 describe('Testing challenge 3', () => {
   test('It should append by modifying the oiginal', () => {
     const a = ['Yes', 'it', 'is'];
     appendFirstToLast(a);
-
     expect(a).toStrictEqual(['Yes', 'it', 'is', 'Yes']);
   });
 });
-
 describe('Testing challenge 4', () => {
   test('It should add a property to an object', () => {
     const a = { fullName: 'Octavia Butler' };
     addBirthYearProperty(a, 1947);
-
     expect(a.yearBorn).toStrictEqual(1947);
   });
 });
-
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
-
     expect(a[0].isAuthor).toStrictEqual(true);
     expect(a[1].isAuthor).toStrictEqual(true);
     expect(a[2].isAuthor).toStrictEqual(true);
   });
 });
-
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
     append(a, b);
-
     expect(a).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
